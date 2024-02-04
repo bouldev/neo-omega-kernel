85f0dcdb16deaee154f3144bcf0b63e063cf2520
CMA2401PT <1634268014@qq.com>
2024-02-04 22:02:22 +0800

    change default port for python to 24015 (same as access point)

--- neomega/c_api/conn.py
+++ neomega/c_api/conn.py
@@ -863,7 +863,7 @@ if __name__ == '__main__':
         # 为什么明明是在本地，还有address? 这个是为了方便远程连接的, 你可以新开一个远程连接 ConnectType.Remote 它会连接到这个进程
         omega=ThreadOmega(
             connect_type=ConnectType.Local,
-            address="tcp://localhost:24017",
+            address="tcp://localhost:24015",
             accountOption=AccountOptions(
                 UserName="2401PT",
                 UserPassword="24*******",
@@ -875,7 +875,7 @@ if __name__ == '__main__':
         # 你需要先运行 python access.py
         omega=ThreadOmega(
             connect_type=ConnectType.Remote,
-            address="tcp://localhost:24017",
+            address="tcp://localhost:24015",
             accountOption=None
         )
 
