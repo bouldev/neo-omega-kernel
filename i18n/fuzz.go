92a1c4c35ce28e2b0e1a246f9539653c4c476261
Liliya233 <79683806+Liliya233@users.noreply.github.com>
2024-05-05 01:05:44 +1000

    Fix typo

--- i18n/fuzz.go
+++ i18n/fuzz.go
@@ -19,7 +19,7 @@ func FuzzyTransErr(err error) error {
 		CheckAndAppend("EOF", "文件或者网络连接(过早)中断,如果没有更多有价值信息,可能无法推断错误真正原因,请尝试联系开发者"),
 		CheckAndAppend("kick", "机器人被从服务器中踢出去了,可能是管理做的或者机器人自己被谁错误封禁了(请删除封禁数据)"),
 		CheckAndAppend("loggedinOtherLocation", "多个程序在使用同一个机器人,请检查是否有多个程序在同一个服务器作业"),
-		CheckAndAppend("Already logged in", "机器人已经登陆 -> 可能是机器人刚刚退出就重建，过几秒再进就好"),
+		CheckAndAppend("Already logged in", "机器人已经登陆 -> 可能是机器人刚刚退出就重进，过几秒再进就好"),
 		CheckAndAppend("error code: 522", "验证服务器已宕机"),
 	}
 	for _, r := range rules {
