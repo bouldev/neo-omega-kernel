61e24d62642311cc2c3af8b68e8248ac6d501762
Liliya233 <79683806+Liliya233@users.noreply.github.com>
2024-06-30 03:17:56 +1000

    fix and improve OperatorChallenge

--- neomega/rental_server_impact/challenges/privilege_challenge.go
+++ neomega/rental_server_impact/challenges/privilege_challenge.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"neo-omega-kernel/i18n"
+	"neo-omega-kernel/minecraft/protocol"
 	"neo-omega-kernel/minecraft/protocol/packet"
 	"neo-omega-kernel/neomega"
 	"time"
@@ -26,22 +27,35 @@ func NewOperatorChallenge(omega neomega.MicroOmega, lostPrivilegeCallBack func()
 		MicroOmega:      omega,
 		lostPrivilegeCB: lostPrivilegeCallBack,
 	}
-	omega.GetGameListener().SetTypedPacketCallBack(packet.IDAdventureSettings, helper.onAdventurePacket, false)
+	omega.GetGameListener().AddNewNoBlockAndDetachablePacketCallBack(
+		map[uint32]bool{
+			packet.IDAddPlayer:       true,
+			packet.IDUpdateAbilities: true,
+		},
+		func(pk packet.Packet) error {
+			switch pk.ID() {
+			case packet.IDAddPlayer:
+				pkt := pk.(*packet.AddPlayer)
+				helper.onPermissionChange(pkt.AbilityData)
+			case packet.IDUpdateAbilities:
+				pkt := pk.(*packet.UpdateAbilities)
+				helper.onPermissionChange(pkt.AbilityData)
+			}
+			return nil
+		},
+	)
 	omega.GetGameListener().SetTypedPacketCallBack(packet.IDSetCommandsEnabled, helper.onSetCommandEnabledPacket, false)
 	return helper
 }
 
-func (o *OperatorChallenge) onAdventurePacket(pk packet.Packet) {
-	p := pk.(*packet.AdventureSettings)
-	if o.GetMicroUQHolder().GetBotBasicInfo().GetBotUniqueID() == p.PlayerUniqueID {
-		if p.PermissionLevel >= packet.PermissionLevelOperator {
+func (o *OperatorChallenge) onPermissionChange(abilityData protocol.AbilityData) {
+	if o.GetMicroUQHolder().GetBotBasicInfo().GetBotUniqueID() == abilityData.EntityUniqueID {
+		if abilityData.CommandPermissions >= packet.CommandPermissionLevelHost {
 			o.hasOpPrivilege = true
-			fmt.Println(i18n.T(i18n.S_bot_operator_privilege_granted))
 		} else {
 			if o.hasOpPrivilege {
 				o.lostPrivilegeCB()
 			}
-			fmt.Println(i18n.T(i18n.S_please_grant_bot_operator_privilege))
 			o.hasOpPrivilege = false
 		}
 	}
@@ -57,19 +71,16 @@ func (o *OperatorChallenge) onSetCommandEnabledPacket(pk packet.Packet) {
 }
 
 func (o *OperatorChallenge) WaitForPrivilege(ctx context.Context) (err error) {
-	time.Sleep(1 * time.Second)
 	for !o.hasOpPrivilege {
+		time.Sleep(1 * time.Second)
 		o.GetGameControl().SendWOCmd("changesetting allow-cheats true")
-		o.GetGameControl().SendWebSocketCmdNeedResponse("tp @s ~~~").AsyncGetResult(func(output *packet.CommandOutput) {
-			if output.SuccessCount > 0 {
-				o.hasOpPrivilege = true
-				o.cheatOn = true
-			}
-		})
+		if o.GetGameControl().SendWebSocketCmdNeedResponse("tp @s ~~~").BlockGetResult().SuccessCount > 0 {
+			o.hasOpPrivilege = true
+			o.cheatOn = true
+		}
 		if ctx.Err() != nil {
 			return fmt.Errorf(i18n.T(i18n.S_bot_operator_privilege_timeout))
 		}
-		time.Sleep(1 * time.Second)
 		if !o.hasOpPrivilege {
 			// o.GetGameControl().BotSay(i18n.T(i18n.S_please_grant_bot_operator_privilege))
 			fmt.Println(i18n.T(i18n.S_please_grant_bot_operator_privilege))
